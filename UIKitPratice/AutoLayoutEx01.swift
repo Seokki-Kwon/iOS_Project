0824e73645f223b677b52a6236ad44deb2e94cfa
KwonSeokki <kwons3327@gmail.com>
2023-11-14 11:20:56 +0900

    AutoLayout 코드구현

---
+++ UIKitPratice/AutoLayoutEx01.swift
@@ -0,0 +1,29 @@
+//
+//  AutoLayoutEx01.swift
+//  UIKitPratice
+//
+//  Created by SeokkiKwon on 11/13/23.
+//
+
+import UIKit
+
+class AutoLayoutEx01: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
